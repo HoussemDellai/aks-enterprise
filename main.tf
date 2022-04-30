57764e12f520f544d5d4526b240ab1ad2c989cb8
Houssem Dellai <houssem.dellai@live.com>
2022-04-30 18:43:14 +0200

    edited tf

--- main.tf
+++ main.tf
@@ -36,6 +36,7 @@ data "azurerm_subnet" "kubesubnet" {
   name                 = var.aks_subnet_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
+
   depends_on           = [azurerm_virtual_network.vnet]
 }
 
@@ -43,6 +44,7 @@ data "azurerm_subnet" "appgwsubnet" {
   name                 = var.app_gateway_subnet_name
   virtual_network_name = azurerm_virtual_network.vnet.name
   resource_group_name  = azurerm_resource_group.rg.name
+  
   depends_on           = [azurerm_virtual_network.vnet]
 }
 
