65cff507eadfba409178872094cb38d8cafddb52
Houssem Dellai <houssem.dellai@live.com>
2022-05-23 14:54:54 +0200

    revert to Azure CNI

---
+++ commands.sh
@@ -0,0 +1,17 @@
+az login
+
+terraform init
+
+terraform fmt
+
+terraform validate
+
+terraform plan -out tfplan
+
+# Format tfplan.json file
+terraform show -json tfplan | jq '.' > tfplan.json
+
+# show only the changes
+cat tfplan.json | jq -r '(.resource_changes[] | [.change.actions[], .type, .change.after.name]) | @tsv'
+
+terraform apply tfplan
