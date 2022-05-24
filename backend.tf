b82c3e0553096290ad8861c22db9f9a6d068ad3c
Houssem Dellai <houssem.dellai@live.com>
2022-05-24 17:42:21 +0200

    hub-spoke changes

---
+++ backend.tf
@@ -0,0 +1,14 @@
+# terraform {
+#   backend "azurerm" {
+#     # resource_group_name  = "tf_state"
+#     # storage_account_name = "tfstate019"
+#     # container_name       = "tfstate"
+#     # key                  = "terraform.tfstate"
+#   }
+# }
+
+terraform {
+  backend "local" {
+    path = "terraform.tfstate"
+  }
+}
