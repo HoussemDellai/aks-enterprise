cafbe45a791777190ba768270fc69c90fea19872
Houssem Dellai <houssem.dellai@live.com>
2022-05-31 09:16:28 +0200

    push before adding Velero

--- providers.tf
+++ providers.tf
@@ -1,11 +1,11 @@
 terraform {
 
-  required_version = ">=1.2.0"
+  required_version = ">=1.2.1"
 
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "~>3.7.0"
+      version = "~>3.8.0"
     }
 
     helm = {
