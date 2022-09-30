3bc0fa8d0271e6d3720cebfff64f9bdcd8503638
Houssem Dellai <hodellai@microsoft.com>
2022-09-30 18:42:52 +0200

    minor fixes

--- providers.tf
+++ providers.tf
@@ -7,12 +7,12 @@ terraform {
 
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = ">= 3.23.0"
+      version = ">= 3.25.0"
     }
 
     azuread = {
       source  = "hashicorp/azuread"
-      version = ">= 2.28.1"
+      version = ">= 2.29.0"
     }
 
     kubernetes = {
@@ -22,12 +22,12 @@ terraform {
 
     helm = {
       source  = "hashicorp/helm"
-      version = ">= 2.6.0"
+      version = ">= 2.7.0"
     }
 
     azapi = {
       source  = "Azure/azapi"
-      version = "0.6.0"
+      version = "1.0.0"
     }
   }
 }
