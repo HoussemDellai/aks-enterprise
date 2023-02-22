f751d3e1db1f2ed7005b4c8eaa54137093d90ef1
Houssem Dellai <hodellai@microsoft.com>
2023-02-22 07:54:51 +0100

    started multi level refactoring

---
+++ 04_spoke_shared/rg.tf
@@ -0,0 +1,5 @@
+resource "azurerm_resource_group" "rg_spoke_shared" {
+  name     = var.rg_spoke_shared
+  location = var.resources_location
+  tags     = var.tags
+}
---
+++ 04_spoke_shared/route_table.tf
@@ -0,0 +1,5 @@
+# resource "azurerm_subnet_route_table_association" "association_route_table_subnet_mgt" {
+#   count          = var.enable_vm_jumpbox_windows || var.enable_vm_jumpbox_linux ? 1 : 0
+#   subnet_id      = azurerm_subnet.subnet_mgt.id
+#   route_table_id = data.terraform_remote_state.hub.outputs.route_table_id # azurerm_route_table.route_table_to_firewall.id
+# }
