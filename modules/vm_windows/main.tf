57d100e0b0ca9a840a2e3ea54e72269632fa8461
Houssem Dellai <hodellai@microsoft.com>
2023-04-29 21:54:53 +0200

    added tf config

--- modules/vm_windows/main.tf
+++ modules/vm_windows/main.tf
@@ -18,7 +18,7 @@ resource azurerm_resource_group rg {
 }
 
 resource azurerm_user_assigned_identity identity_vm {
-  name                = "identity-vm-windows"
+  name                = "identity-vm"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   tags                = var.tags
